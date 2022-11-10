46cd6a3038d5a4885ed60c8933c31ab25ee149fc
tai-phan <phantuantai7293@gmail.com>
2022-11-10 16:38:06 +0700

    next video 119

---
+++ controllers/errorController.js
@@ -0,0 +1,54 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  }
+  // Programming or other unknown error: don't leak error details
+  else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.name = err.name; // trick to fix the bug
+    // console.log('original error: ', err); // error cloning object
+    // console.log('clone error: ', error); // error cloning object
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    sendErrorProd(error, res);
+  }
+};
